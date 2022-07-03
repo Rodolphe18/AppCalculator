c7f89676b89936466bf3b76ef0fdfe50925e55aa
Rodolphe18 <rodolphefrancotte18@gmail.com>
2022-07-03 19:47:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculatorapp/CalculatorAction.kt
@@ -0,0 +1,11 @@
+import com.example.calculatorapp.CalculatorOperation
+
+
+sealed class CalculatorAction {
+    data class Number(val number: Int): CalculatorAction()
+    object Clear: CalculatorAction()
+    object Delete: CalculatorAction()
+    data class Operation(val operation: CalculatorOperation): CalculatorAction()
+    object Calculate: CalculatorAction()
+    object Decimal: CalculatorAction()
+}
